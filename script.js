856cb3889eb4237cd7b0bf432ef72a56856f1e66
Bhanu-183 <bhanukrishna2019@iiitkottayam.ac.in>
2021-08-07 00:35:02 +0530

    Inital Commit

--- script.js
+++ script.js
@@ -4,7 +4,18 @@ function navchange(evt, navname) {
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].className = tabcontent[i].className.replace(" active", "");
     }
-
     document.getElementById(navname).className += " active";
     // evt.currentTarget.className += " active";
 }
+
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll > 400) {
+            $(".navbar").css("background", "white");
+        } else {
+            $(".navbar").css("background", "rgba(0,0,0,0)");
+        }
+    });
+});
