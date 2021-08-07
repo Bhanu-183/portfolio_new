74eb9eaf64d41d16af800a3e4fed4678f305496c
Bhanu-183 <bhanukrishna2019@iiitkottayam.ac.in>
2021-08-07 10:43:02 +0530

    Resume added

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function navchange(evt, navname) {
     var i, tablinks;
-    tabcontent = document.getElementsByClassName("nav-link");
+    tabcontent = document.getElementsByClassName("nav-item");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].className = tabcontent[i].className.replace(" active", "");
     }
@@ -8,11 +8,21 @@ function navchange(evt, navname) {
     // evt.currentTarget.className += " active";
 }
 
-$(document).ready(function () {
+function sidenav(evt, navname) {
+    var i, tablinks;
+    tabcontent = document.getElementsByClassName("side-link");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].className = tabcontent[i].className.replace(" activated", "");
+    }
+    document.getElementById(navname).className += " activated";
+}
 
+$(document).ready(function () {
     $(window).scroll(function () {
+        // alert("Helo");
         var scroll = $(window).scrollTop();
-        if (scroll > 400) {
+        if (scroll > 200) {
+
             $(".navbar").css("background", "white");
         } else {
             $(".navbar").css("background", "rgba(0,0,0,0)");
